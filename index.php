1065bd3f28760265897c70b569e2c5f657957fb0
CintiaLara <cintialaraandrade@gmail.com>
2019-03-11 16:27:23 -0400

    Upload Index

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+<title>Carrinho de compras</title>
+</head>
+  <body>
+
+<?php
+echo "<h1> Bem-vindo ao Comprinhas daHora </h1>";
+
+ ?>
+
+</body>
+
+</html>
---
+++ read.md
@@ -0,0 +1,1 @@
+#Primeiro trabalho de LPC
