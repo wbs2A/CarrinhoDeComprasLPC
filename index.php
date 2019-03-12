c2f0e37227101081702e0bfc7d0f980c42544ba6
wbs2a <barbosawesley101@gmail.com>
2019-03-12 14:48:02 -0400

    Alteração das estrutura do projeto

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
-<html>
-
-<head>
-<title>Carrinho de compras</title>
-</head>
-  <body>
-
 <?php
-echo "<h1> Bem-vindo ao Comprinhas daHora </h1>";
-
- ?>
-
-</body>
-
-</html>
+session_start();
+header('location:views/index.php');
+?>
