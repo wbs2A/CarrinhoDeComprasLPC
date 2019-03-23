3fb4a68908e1c24052bf9fb2d2d3f7ab4d1339fd
ValdenizeRibeiro <valdenizesilva8@gmail.com>
2019-03-23 16:17:23 -0300

    perfil

--- views/perfil.php
+++ views/perfil.php
@@ -2,18 +2,10 @@
 include 'header.php';
 include 'navbar.php';
 ?>
+
 <?php
-	$ch = curl_init();
-	$optArray = array(
-			CURLOPT_URL => "http://localhost/CarrinhoDeComprasLPC/views/controller.php?allUser",
-			CURLOPT_RETURNTRANSFER => true
-	);
-	// apply those options
-	curl_setopt_array($ch, $optArray);
-	$t=curl_exec($ch);
-	$j=json_decode($t);
-	var_dump($j);
-	curl_close($ch);
+	include '../model/App/ModelUser.php';
+	$info=(new ModelUser)->infoUser();
 
 echo '<script type="text/javascript" src="../js/registro.js"></script>
 <div class="main_slider criar p-1">
@@ -29,39 +21,46 @@ echo '<script type="text/javascript" src="../js/registro.js"></script>
 										<div class="row">
 							          <div class="col-md-8 mb-3">
 							            <label for="nome">Nome Completo</label>
-							            <input type="text" class="form-control" name="nome" id="nome" placeholder="Nome completo">
+													<input type="text" class="form-control" name="nome" id="nome" placeholder="Nome completo" disabled value=" ' . $info->user->NomeComp . ' ">
 							        </div>
 							        <div class="col-md-4 mb-3">
 							        <label for="email">Email</label>
-							        <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
+							        <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" disabled value=" ' . $info->email . ' ">
 							      </div>
 							      </div>
 							      <div class="row">
 							    <div class="col-md-4 mb-3">
 							      <label for="nascimento">Data de nascimento</label>
-							      <input type="date" class="form-control" id="nascimento" name="nascimento" placeholder="">
+							      <input type="date" class="form-control" id="nascimento" name="nascimento" placeholder="" disabled value=" ' . $info->nascimento . ' ">
 							    </div>
-							        <div class="col-md-4 mb-3">
-							      <label for="telefone">Telefone</label>
-							      <input type="tel" class="form-control" id="telefone" aria-describedby="telefoneHelp" name="telefone" placeholder="">
-							      <small id="emailHelp" class="form-text text-muted">Por favor, insira apenas numeros.</small>
-							    </div>
-							    <div class="col-md-4 mb-3">
-							      <label for="tipo">Tipo</label>
-							       <select id="tipo" name="tipo" class="form-control">
-							        <option>fixo</option>
-							        <option>celular</option>
-							      </select>
-							    </div>
-							      </div>
+									<div class="row">
+									';
+									  foreach ($info->telefone as $key => $value2) {
+
+							      echo '
+
+											<div class="col-md-3 mb-3">
+									      <label for="telefone">Telefone</label>
+									      <input type="tel" class="form-control" id="telefone" aria-describedby="telefoneHelp" name="telefone" placeholder="" disabled value=" ' . $value2->telefone . ' ">
+									    </div>
+
+									    <div class="col-md-3 mb-3">
+									      <label for="tipo">Tipo</label>
+									       <select id="tipo" name="tipo" class="form-control">
+									        <option  disabled selected>'.$value2->tipo.'</option>
+									      </select>
+									    </div>
+								';
+}
+									echo'</div></div>
 							  <div class="row">
 							        <div class="col-md-6 mb-3">
 							          <label for="cpf">CPF</label>
-							          <input type="number" class="form-control" id="cpf" maxlength="14" name="cpf" placeholder="CPF" value="" required>
+							          <input type="text" class="form-control" id="cpf" maxlength="14" name="cpf" placeholder="CPF" disabled value=" ' . $info->user->cpf . ' " required>
 							        </div>
 							        <div class="col-md-6 mb-3">
 							          <label for="rg">RG</label>
-							          <input type="text" class="form-control" name="rg" id="rg" placeholder="" value="" required="">
+							          <input type="text" class="form-control" name="rg" id="rg" placeholder=""  required="" disabled value=" ' . $info->rg . '">
 							        </div>
 							      </div>
 								 </fieldset>
@@ -72,36 +71,36 @@ echo '<script type="text/javascript" src="../js/registro.js"></script>
 							     <div class="col-md-6 mb-3">
 							       <label for="pais">Pais</label>
 							       <select class="form-control" id="pais" name="pais">
-
+											 <option disabled selected> ' . $info->endereco->pais . '</option>
 							       </select>
 							     </div>
 							     <div class="col-md-6 mb-3">
 							       <label for="cep">CEP</label>
-							       <input type="text" class="form-control" id="cep" name="cep" placeholder="">
+							       <input type="text" class="form-control" id="cep" name="cep" placeholder="" disabled value=" ' . $info->endereco->cep . '">
 							     </div>
 							       </div>
 							       <div class="row">
 							     <div class="col-md-4 mb-3">
 							       <label for="numero">Numero</label>
-							       <input type="number" class="form-control" id="numero" name="numero" placeholder="">
+							       <input type="text" class="form-control" id="numero" name="numero" placeholder="" disabled value=" ' . $info->endereco->numero . '">
 							     </div>
 							     <div class="col-md-4 mb-3">
 							       <label for="bairro">Bairro</label>
-							       <input type="text" class="form-control" id="bairro" name="bairro" placeholder="">
+							       <input type="text" class="form-control" id="bairro" name="bairro" placeholder="" disabled value=" ' . $info->endereco->bairro . '">
 							     </div>
 							     <div class="col-md-4 mb-3">
 							       <label for="rua">Rua</label>
-							       <input type="text" class="form-control" id="rua" name="rua" placeholder="">
+							       <input type="text" class="form-control" id="rua" name="rua" placeholder="" disabled value=" ' . $info->endereco->rua . '">
 							     </div>
 							       </div>
 							       <div class="row">
 							     <div class="col-md-6 mb-3">
 							       <label for="estado">Estado</label>
-							       <input type="text" class="form-control" id="estado" name="estado" placeholder="">
+							       <input type="text" class="form-control" id="estado" name="estado" placeholder="" disabled value=" ' . $info->endereco->estado . '">
 							     </div>
 							     <div class="col-md-6 mb-3">
 							       <label for="cidade">Cidade</label>
-							       <input type="text" class="form-control" id="cidade" name="cidade" placeholder="">
+							       <input type="text" class="form-control" id="cidade" name="cidade" placeholder="" disabled value=" ' . $info->endereco->cidade . '">
 							     </div>
 							       </div>
    								 </fieldset>
@@ -110,15 +109,15 @@ echo '<script type="text/javascript" src="../js/registro.js"></script>
   							        <div class="row">
   							          <div class="col-md-4 mb-3">
   							               <label for="numero-cartao">Número</label>
-  							               <input type="number" class="form-control" id="numero-cartao" name="numero-cartao">
+  							               <input type="text" class="form-control" id="numero-cartao" name="numero-cartao" disabled value=" ' . $info->pagamento->nCartao . '">
   							           </div>
   							          <div class="col-md-3 mb-3">
-  							            <label for="vcc">VCC</label>
-  							            <input type="number" class="form-control" id="vcc" name="vcc" placeholder="">
+  							            <label for="vcc">CVV</label>
+  							            <input type="text" class="form-control" id="vcc" name="vcc" placeholder="" disabled value=" ' . $info->pagamento->cvvCartao . '">
   							          </div>
   							           <div class="col-md-5 mb-3">
   							               <label for="validade-cartao">Validade</label>
-  							               <input type="month" class="form-control" id="validade-cartao" name="validade-cartao">
+  							               <input type="date" class="form-control" id="validade-cartao" name="validade-cartao" disabled value=" ' . $info->pagamento->vencimentoCartao . '">
   							           </div>
   							       </div>
   							     </fieldset>
@@ -128,6 +127,7 @@ echo '<script type="text/javascript" src="../js/registro.js"></script>
 				    </div>
 				</div>';
 ?>
+
 <?php
 include 'footer.php';
 ?>
